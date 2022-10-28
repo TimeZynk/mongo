ab313292aa71713b992f5ff902aebf427f20b33f
lars-timezynk <58430920+lars-timezynk@users.noreply.github.com>
2022-10-28 10:57:57 +0200

    Fix to arraylist conversion. (#7)
    Co-authored-by: Lars <lars.melander@gmail.com>

--- test/com/timezynk/mongo/test/convert.clj
+++ test/com/timezynk/mongo/test/convert.clj
@@ -1,7 +1,11 @@
 (ns com.timezynk.mongo.test.convert
   (:require
    [clojure.test :refer [deftest is testing]]
-   [com.timezynk.mongo.utils.convert :refer [clj->doc doc->clj]]))
+   [com.timezynk.mongo.utils.convert :refer [clj->doc doc->clj]])
+  (:import [clojure.lang PersistentArrayMap PersistentVector]
+           [java.lang String]
+           [java.util ArrayList]
+           [org.bson BsonString Document]))
 
 (deftest clj->doc-tests
   (testing "Can convert keyword"
@@ -15,8 +19,14 @@
 
 (deftest doc->clj-tests
   (testing "Can convert keyword"
-    (is (= {:testword 1} (doc->clj (doto (org.bson.Document.) (.append "testword" 1))))))
+    (is (= {:testword 1} (doc->clj (.append (Document.) "testword" 1)))))
   (testing "Can convert keyword with slash"
-    (is (= {:my/testword 1} (doc->clj (doto (org.bson.Document.) (.append "my/testword" 1))))))
+    (is (= {:my/testword 1} (doc->clj (.append (Document.) "my/testword" 1)))))
   (testing "Can convert keyword with double slashes"
-    (is (= {(keyword "my/test/word") 1} (doc->clj (doto (org.bson.Document.) (.append "my/test/word" 1)))))))
+    (is (= {(keyword "my/test/word") 1} (doc->clj (.append (Document.) "my/test/word" 1)))))
+  (testing "Can convert ArrayLists"
+    (let [list (doc->clj (ArrayList. [(.append (Document.) "a" 1)
+                                      (BsonString. "ABC")]))]
+      (is (= PersistentVector (type list)))
+      (is (= [{:a 1} "ABC"] list))
+      (is (= [PersistentArrayMap String] (map type list))))))
