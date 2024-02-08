fde4cbeac3f770c59e4a0fd3c0699105ae86f924
Lars <lars.melander@gmail.com>
2024-02-08 08:43:39 +0100

    Improved testing a bit.

--- test/com/timezynk/mongo/test/modify_schema.clj
+++ test/com/timezynk/mongo/test/modify_schema.clj
@@ -1,17 +1,35 @@
 (ns com.timezynk.mongo.test.modify-schema
   (:require
-   [clojure.test :refer [deftest is testing use-fixtures]]
-   [clojure.tools.logging :as log]
+   [clojure.test :refer [deftest is use-fixtures]]
    [com.timezynk.mongo :as m]
    [com.timezynk.mongo.schema :as s]
-   [com.timezynk.mongo.test.utils.db-utils :as dbu])
-  (:import [org.bson.types ObjectId]))
+   [com.timezynk.mongo.test.utils.db-utils :as dbu]))
 
 (use-fixtures :once #'dbu/test-suite-db-fixture)
 (use-fixtures :each #'dbu/test-case-db-fixture)
 
-(deftest modify
-  (is true)
-  (m/create-collection! :user :schema {:name (s/string)} :validation {:name "Boo"})
-  (log/spy (m/collection-info :user))
-  (m/modify-collection! :user :schema {:name (s/string)}))
+(deftest check-validation
+  (m/create-collection! :user
+                        :schema {:name (s/string)}
+                        :validation {:name "Boo"})
+  (is (= {:options {:validator {:$jsonSchema {:bsonType "object"
+                                              :properties {:_id  {:bsonType "objectId"}
+                                                           :name {:bsonType "string"}}
+                                              :additionalProperties false
+                                              :required ["name"]}
+                                :name "Boo"}
+                    :validationLevel "strict"
+                    :validationAction "error"}}
+         (-> (m/collection-info :user)
+             (select-keys [:options]))))
+  (m/modify-collection! :user :schema {:name (s/string)})
+  (is (= {:options {:validator {:$jsonSchema {:bsonType "object"
+                                              :properties
+                                              {:_id {:bsonType "objectId"}
+                                               :name {:bsonType "string"}}
+                                              :additionalProperties false
+                                              :required ["name"]}}
+                    :validationLevel "strict"
+                    :validationAction "error"}}
+         (-> (m/collection-info :user)
+             (select-keys [:options])))))
